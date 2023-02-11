f8cb0036e3d2718a7ef6a7c520bc44f2cd0c6557
André <aldeoliveira@gmail.com>
2023-02-11 20:17:09 -0300

    feat: connect to render

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'render/index'
   resources :mining_types
   get 'welcome/index' # , to: 'welcome#index'
   
@@ -7,10 +8,12 @@ Rails.application.routes.draw do
   # post '/coins', to: 'coins#create'
   # ...
 
-  root to: 'welcome#index'
+  # root to: 'welcome#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'render#index'
 end
